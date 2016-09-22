187c20c205ff475485399ca712be8822be060490
Jared Hanson <jaredhanson@gmail.com>
2016-09-21 20:20:25 -0700

    Reorganize plugins.

--- plugins/auth/local.js
+++ plugins/auth/local.js
@@ -1,16 +1,3 @@
-function factory(verifyPassword) {
-  var Strategy = require('passport-local');
-  
-  var strategy = new Strategy(verifyPassword);
-  return strategy;
-}
-
-factory['@implements'] = 'http://i.bixbyjs.org/http/auth/Scheme';
-factory['@require'] = [
-  'http://i.bixbyjs.org/aaa/verifyPasswordFunc'
-];
-
-
 exports = module.exports = {
-  'local': factory,
+  'scheme': require('./local/scheme'),
 };
---
+++ plugins/auth/local/scheme.js
@@ -0,0 +1,11 @@
+exports = module.exports = function(verifyPassword) {
+  var Strategy = require('passport-local');
+  
+  var strategy = new Strategy(verifyPassword);
+  return strategy;
+};
+
+exports['@implements'] = 'http://i.bixbyjs.org/http/auth/Scheme';
+exports['@require'] = [
+  'http://i.bixbyjs.org/aaa/verifyPasswordFunc'
+];
