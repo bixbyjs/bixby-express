6c8dac455f424d3a0e9215c4f8cb6f1eac9c2345
Jared Hanson <jaredhanson@gmail.com>
2016-09-21 21:47:12 -0700

    Slim down manifest of OAuth plugin.

--- plugins/auth/oauth.js
+++ plugins/auth/oauth.js
@@ -1,6 +1,12 @@
 exports = module.exports = {
-  'scheme': require('./oauth/scheme'),
-  'clientcb': require('./oauth/clientcb'),
-  'tokencb': require('./oauth/tokencb'),
-  'validatecb': require('./oauth/validatecb'),
+  'scheme': require('./oauth/scheme')
+};
+
+exports.load = function(id) {
+  try {
+    return require('./oauth' + id);
+  } catch (ex) {
+    if (ex.code == 'MODULE_NOT_FOUND') { return; }
+    throw ex;
+  }
 };
