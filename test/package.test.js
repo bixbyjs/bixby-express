c296696639046c0fbce157cfb5093140a7693e0f
Jared Hanson <jaredhanson@gmail.com>
2018-02-14 13:09:37 -0800

    Add tests.

--- test/package.test.js
+++ test/package.test.js
@@ -2,6 +2,17 @@
 
 describe('bixby-express', function() {
   
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      //expect(json.assembly.namespace).to.equal('http');
+      
+      expect(json.assembly.components).to.have.length(11);
+      expect(json.assembly.components).to.include('middleware/authenticate');
+    });
+  });
+  
   it('should throw if required', function() {
     expect(function() {
       var pkg = require('..');
