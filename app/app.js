ad68b4fd198cc1fd030358917762055fea0668e3
Jared Hanson <jaredhanson@gmail.com>
2018-02-14 16:52:12 -0800

    Make gateway configuration more dynamic.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
-exports = module.exports = function(IoC, service, settings, logger) {
+exports = module.exports = function(IoC, gateways, service, settings, logger) {
   var normalizePort = require('../lib/utils').normalizePort;
   
-  // TODO: Make this adaptable to the runtime environment (CGI, Lambda, etc)
   
   // TODO: Dynamically create the Application object, and if it is not available,
   //       fallback to loading an internal one, which automatically `use`s
@@ -23,29 +22,42 @@ exports = module.exports = function(IoC, service, settings, logger) {
       throw err;
     })
     */
-    .then(function() { return IoC.create('http://i.bixbyjs.org/http/Server'); })
-    .then(function(server) {
-      // Dispatch requests to the service, which in this case is an Express app.
-      server.on('request', service);
-  
-      server.once('listening', function() {
-        var addr = this.address();
-        logger.info('HTTP server listening on %s:%d', addr.address, addr.port);
-      });
-      
-      var opts = settings.get('http/server') || {};
+    .then(function() {
+      var gatewayIfaces = IoC.components(gateways());
     
-      var address = opts.address;
-      var port = opts.port !== undefined ? opts.port : (normalizePort(process.env.PORT) || 8080);
-      
-      server.listen(port, address);
-    });
+      return Promise.all(gatewayIfaces.map(function(iface) { return iface.create(); } ))
+        .then(function(gateways) {
+          gateways.forEach(function(gateway, i) {
+            //console.log(gateway);
+            //console.log(i);
+            
+            var server = gateway;
+            
+            // Dispatch requests to the service, which in this case is an Express app.
+            server.on('request', service);
   
+            server.once('listening', function() {
+              var addr = this.address();
+              logger.info('HTTP server listening on %s:%d', addr.address, addr.port);
+            });
+      
+            var opts = settings.get('http/server') || {};
+    
+            var address = opts.address;
+            var port = opts.port !== undefined ? opts.port : (normalizePort(process.env.PORT) || 8080);
+      
+            server.listen(port, address);
+            
+            
+          });
+        });
+    });
 };
 
 exports['@singleton'] = true;
 exports['@require'] = [
   '!container',
+  './gateways',
   'http://i.bixbyjs.org/http/Service',
   'http://i.bixbyjs.org/Settings',
   'http://i.bixbyjs.org/Logger'
