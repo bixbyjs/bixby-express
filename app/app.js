1650d0ef46cf5ffc09dd52a9fce70aafb00c7dc9
Jared Hanson <jaredhanson@gmail.com>
2018-02-17 10:53:37 -0800

    Add service discovery.

--- app/app.js
+++ app/app.js
@@ -9,6 +9,8 @@ exports = module.exports = function(IoC, logger) {
     .then(function(service) {
       return IoC.create('./platform/gateways')
         .then(function(gateways) {
+          // TODO: Implement a way to return the annotations, so those
+          //       can be used to drive service discovery.
           gateways.forEach(function(gateway, i) {
             // Dispatch requests to the service, which in this case is an
             // Express app.
