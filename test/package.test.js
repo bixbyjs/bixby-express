aefb87ca3008866ab9729785ecefa2a0013c2156
Jared Hanson <jaredhanson@gmail.com>
2018-02-26 16:18:01 -0800

    Test assembly namespace.

--- test/package.test.js
+++ test/package.test.js
@@ -6,7 +6,7 @@ describe('bixby-express', function() {
     var json = require('../package.json');
     
     it('should have assembly metadata', function() {
-      //expect(json.assembly.namespace).to.equal('http');
+      expect(json.assembly.namespace).to.equal('http');
       
       expect(json.assembly.components).to.have.length(12);
       expect(json.assembly.components).to.include('middleware/authenticate');
