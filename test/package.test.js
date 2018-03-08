92135c9a45f20a3caed9db456a1739293c17a2ba
Jared Hanson <jaredhanson@gmail.com>
2018-03-07 18:33:30 -0800

    Update tests.

--- test/package.test.js
+++ test/package.test.js
@@ -9,7 +9,17 @@ describe('bixby-express', function() {
       expect(json.assembly.namespace).to.equal('http');
       
       expect(json.assembly.components).to.have.length(11);
+      expect(json.assembly.components).to.include('middleware/state/flow');
       expect(json.assembly.components).to.include('middleware/authenticate');
+      expect(json.assembly.components).to.include('middleware/cors');
+      expect(json.assembly.components).to.include('middleware/csrfprotection');
+      expect(json.assembly.components).to.include('middleware/errorlogging');
+      expect(json.assembly.components).to.include('middleware/initialize');
+      expect(json.assembly.components).to.include('middleware/logging');
+      expect(json.assembly.components).to.include('middleware/parse');
+      expect(json.assembly.components).to.include('middleware/parsecookies');
+      expect(json.assembly.components).to.include('state/store');
+      expect(json.assembly.components).to.include('app');
     });
   });
   
