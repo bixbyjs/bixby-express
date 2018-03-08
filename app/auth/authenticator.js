36ff313448040b8733e1c9cfa931ff1ae4b35588
Jared Hanson <jaredhanson@gmail.com>
2018-03-08 07:44:37 -0800

    Don't load auth providers.

--- app/auth/authenticator.js
+++ app/auth/authenticator.js
@@ -8,28 +8,13 @@ exports = module.exports = function(IoC, logger) {
   return Promise.resolve(authenticator)
     .then(function(authenticator) {
       // Register HTTP authentication schemes.
-      var schemeComps = IoC.components('http://i.bixbyjs.org/http/auth/Scheme');
+      var components = IoC.components('http://i.bixbyjs.org/http/auth/Scheme');
       
-      return Promise.all(schemeComps.map(function(comp) { return comp.create(); } ))
+      return Promise.all(components.map(function(comp) { return comp.create(); } ))
         .then(function(schemes) {
           schemes.forEach(function(scheme, i) {
-            authenticator.use(schemeComps[i].a['@scheme'] || scheme.name, scheme);
-            logger.info('Loaded HTTP authentication scheme: ' + (schemeComps[i].a['@scheme'] || scheme.name));
-          });
-        })
-        .then(function() {
-          return authenticator;
-        });
-    })
-    .then(function(authenticator) {
-      // Register federated identity providers.
-      var providerComps = IoC.components('http://i.bixbyjs.org/http/auth/Provider');
-      
-      return Promise.all(providerComps.map(function(comp) { return comp.create(); } ))
-        .then(function(providers) {
-          providers.forEach(function(provider, i) {
-            authenticator.use(providerComps[i].a['@provider'], provider);
-            logger.info('Loaded federated identity provider: ' + (providerComps[i].a['@provider']));
+            authenticator.use(components[i].a['@scheme'] || scheme.name, scheme);
+            logger.info('Loaded HTTP authentication scheme: ' + (components[i].a['@scheme'] || scheme.name));
           });
         })
         .then(function() {
@@ -54,7 +39,7 @@ exports = module.exports = function(IoC, logger) {
     .then(function(authenticator) {
       return authenticator;
     });
-}
+};
 
 exports['@singleton'] = true;
 exports['@require'] = [ '!container', 'http://i.bixbyjs.org/Logger' ];
