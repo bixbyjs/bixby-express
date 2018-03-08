eeb66f443d87312aedcfb3c44639f2687fdc9d76
Jared Hanson <jaredhanson@gmail.com>
2018-03-08 07:39:26 -0800

    Add tests.

---
+++ test/platform/gateway/http.test.js
@@ -0,0 +1,17 @@
+/* global describe, it, expect */
+
+var factory = require('../../../app/platform/gateway/http');
+
+
+describe('platform/gateway/http', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.equal(undefined);
+    expect(factory['@singleton']).to.equal(undefined);
+  });
+  
+});
