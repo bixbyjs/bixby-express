3e81f1cd1f0f53e3d102a44d165b53f460ee5bb9
Jared Hanson <jaredhanson@gmail.com>
2018-12-03 11:04:02 -0800

    Handle no records from service discovery.

--- app/session/store/redis/discover.js
+++ app/session/store/redis/discover.js
@@ -3,6 +3,7 @@ exports = module.exports = function(sd) {
   return function(cb) {
     sd.resolve('_sess-redis._tcp', 'SRV', function(err, records) {
       if (err) { return cb(err); }
+      if (!records) { return cb(null); }
       
       records = records.map(function(rec) {
         return {
