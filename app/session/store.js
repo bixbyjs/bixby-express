01b07f96b3f50a62e12c27b3e70f29f53f020ec5
Jared Hanson <jaredhanson@gmail.com>
2019-05-16 15:43:53 -0700

    Simplify connection to session store.

--- app/session/store.js
+++ app/session/store.js
@@ -1,75 +1,20 @@
-exports = module.exports = function(IoC, logger) {
+exports = module.exports = function(IoC, connect, logger) {
   var uri = require('url');
   
-  return Promise.resolve()
-    .then(function discoverService() {
-      var components = IoC.components('http://i.bixbyjs.org/http/session/StoreDiscoverFunc');
   
-      return Promise.all(components.map(function(c) { return c.create(); } ))
-        .then(function(funcs) {
-          return new Promise(function(resolve, reject) {
-            
-            // Iterate over each of the `discover` functions, attempting to
-            // locate an HTTP session store via supported protocols.
-            (function iter(i) {
-              var func = funcs[i];
-              if (!func) {
-                // Reject with `ENOTFOUND`, causing a jump to the next catch
-                // function.  This function will attempt to create a suitable
-                // session store based on the host environment.
-                return reject('ENOTFOUND');
-              }
-          
-              logger.debug('Discovering HTTP session store via ' + components[i].a['@service']);
-              func(function(err, records, ctx) {
-                if (err && err.code == 'ENOTFOUND') {
-                  // Unable to locate a session store using this particular
-                  // protocol.  Continue discovery using remaining supported
-                  // protocols.
-                  return iter(i + 1);
-                } else if (err) {
-                  return reject(err);
-                }
-                
-                if (!records) { return iter(i + 1); }
-                return resolve([records, ctx]);
-              });
-            })(0);
-            
-          });
-        });
-    })
-    .then(function createStore(args) {
-      var records = args[0]
-        , ctx = args[1] || {};
-      
-      // Iterate over the available components that support the HTTP session
-      // store interface, and create an instance that is compatible with the
-      // protocol found via service discovery.
-      var components = IoC.components('http://i.bixbyjs.org/http/session/Store')
-        , rec = records[0]
-        , component, i, len
-        , url, scheme;
-      
-      for (i = 0, len = components.length; i < len; ++i) {
-        component = components[i];
-        if (component.a['@service'] == ctx.service && component.a['@protocol'] == ctx.protocol) {
-          logger.debug('Connecting to HTTP session store via ' + ctx.service);
-
-          if (rec.url) {
-            url = uri.parse(rec.url);
-            scheme = url.protocol.slice(0, -1);
-            if (component.a['@uri-scheme'] == scheme) {
-              return component.create({ url: rec.url });
-            }
-          }
-        }
-      }
-      
-      // TODO: Throw an error.
-    })
-    .catch(function createDevelopmentStoreIfOK(err) {
-      if (err !== 'ENOTFOUND') { throw err; }
+  var promise = new Promise(function(resolve, reject) {
+    // TODO: Introspect container for session service types.
+    var type = 'sess-redis';
+    
+    connect([ type ], function(err, conn) {
+      if (err) { reject(err); }
+      resolve(conn);
+    });
+  });
+  
+  return promise
+    .catch(function(err) {
+      if (err.code !== 'ENOTFOUND') { throw err; }
       if (process.env.NODE_ENV !== 'development') {
         // TODO: augment this error with supported protocols, for auto-configuration
         throw new Error('Unable to create HTTP session store');
@@ -84,5 +29,6 @@ exports = module.exports = function(IoC, logger) {
 exports['@singleton'] = true;
 exports['@require'] = [
   '!container',
+  'http://i.bixbyjs.org/ns/connect',
   'http://i.bixbyjs.org/Logger'
 ];
