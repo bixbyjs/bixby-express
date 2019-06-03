9a705eb60e7bb2c77ac029d39d115653ac9e648a
Jared Hanson <jaredhanson@gmail.com>
2019-06-02 17:22:07 -0700

    Setup csrf protection with cookie or session, as needed.

--- app/middleware/csrfprotection.js
+++ app/middleware/csrfprotection.js
@@ -1,13 +1,29 @@
-exports = module.exports = function() {
+exports = module.exports = function(store, session, parseCookies) {
   
   // https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)
   // https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet
   // https://en.wikipedia.org/wiki/Cross-site_request_forgery
   
-  return function(options) {
-    return require('csurf')(options);
+  return function() {
+    //var opts = {
+      //cookie: true
+      //cookie: { signed: true }
+    //};
+    
+    var opts = {};
+    if (!store) { opts.cookie = true; }
+    
+    return [
+      store ? session() : parseCookies(),
+      require('csurf')(opts)
+    ];
   };
 };
 
 exports['@implements'] = 'http://i.bixbyjs.org/http/middleware/csrfProtection';
 exports['@singleton'] = true;
+exports['@require'] = [
+  '../session/store',
+  'http://i.bixbyjs.org/http/middleware/session',
+  'http://i.bixbyjs.org/http/middleware/parseCookies'
+];
