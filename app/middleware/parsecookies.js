b0ec7670f39ea48108df0e922c6c91d5613bb00d
Jared Hanson <jaredhanson@gmail.com>
2019-06-02 17:01:02 -0700

    Fetch secret for signed cookies.

--- app/middleware/parsecookies.js
+++ app/middleware/parsecookies.js
@@ -1,9 +1,25 @@
-exports = module.exports = function() {
+exports = module.exports = function(keyring) {
   
-  return function() {
-    return require('cookie-parser')();
-  };
+  return new Promise(function(resolve, reject) {
+    var hostname = 'www';
+  
+    // NOTE: secret will be shared between this and express-session, due to same hostname
+    keyring.get(hostname, function(err, cred) {
+      if (err) { return reject(err); }
+      if (!cred) { return reject(new Error("Cannot find credentials for '" + hostname + "'")); }
+      
+      var secret = cred.password;
+      
+      resolve(function() {
+        return require('cookie-parser')(secret);
+      });
+    });
+    
+  }); // new Promise
 };
 
 exports['@implements'] = 'http://i.bixbyjs.org/http/middleware/parseCookies';
 exports['@singleton'] = true;
+exports['@require'] = [
+  'http://i.bixbyjs.org/security/CredentialsStore'
+];
