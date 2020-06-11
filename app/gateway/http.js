a9e60bf7709909020ba7eed53363b6a4440787f0
Jared Hanson <jaredhanson@gmail.com>
2020-06-10 17:10:07 -0700

    Wrap HTTP server in gateway.

--- app/gateway/http.js
+++ app/gateway/http.js
@@ -1,8 +1,33 @@
 exports = module.exports = function(IoC) {
   
-  return function() {
-    return IoC.create('http://i.bixbyjs.org/http/Server');
-  };
+  return IoC.create('http://i.bixbyjs.org/http/Server')
+    .then(function(server) {
+      
+      return {
+        listen: function(cb) {
+          // --http-server-address --http-server-port
+          // settings.get('http/server/address');
+          //console.log(settings.toObject());
+          //console.log(settings.get('http'));
+          
+          server.once('listening', function() {
+            //var addr = this.address();
+            //logger.info('HTTP server listening on %s:%d', addr.address, addr.port);
+            
+            console.log('SERVER LISTENING!!!');
+          });
+          
+          //var options = settings.get('http/server') || {};
+          var options = {};
+          var address = options.address;
+          //var port = options.port !== undefined ? options.port : normalizePort(process.env.PORT || 8080);
+          var port = 8080;
+          
+          server.listen(port, address);
+        }
+      }
+    });
 };
 
+exports['@implements'] = 'http://i.bixbyjs.org/http/Gateway';
 exports['@require'] = [ '!container' ];
