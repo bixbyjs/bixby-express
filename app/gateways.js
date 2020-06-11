e3acfb0722f493903d4b3a717aaa1c3f3fc8555d
Jared Hanson <jaredhanson@gmail.com>
2020-06-11 16:00:27 -0700

    Document gateways component.

--- app/gateways.js
+++ app/gateways.js
@@ -1,3 +1,23 @@
+/**
+ * WWW gateways.
+ *
+ * This component provides the gateways between the application and the World
+ * Wide Web (WWW).
+ *
+ * This component introspects the application for any components that implement
+ * the HTTP gateway interface and instantiates them, in order to facilitate the
+ * dispatching of requests to the application.
+ *
+ * Depending on the environment and available packages, the communication
+ * protocol may be CGI, FastCGI, SCGI, or similar.  By default, and most
+ * typically, the communication protocol will be HTTP itself.  In production
+ * environments, the application will be upstream from an HTTP proxy which
+ * forwards requests to the application via the gateway protocol.
+ *
+ * Usually only a single gateway interface will be utilized.  However, multiple
+ * interfaces can be in use simultanously, in infrastructures that support such
+ * functionality.
+ */
 exports = module.exports = function(IoC) {
   
   return new Promise(function(resolve, reject) {
@@ -8,6 +28,8 @@ exports = module.exports = function(IoC) {
       var component = components[i];
       if (!component) {
         if (gateways.length == 0) {
+          // No gateways were instantiated, instatiate an HTTP gateway by
+          // default.
           return resolve(Promise.all([ IoC.create('./gateway/http') ]));
         }
         
