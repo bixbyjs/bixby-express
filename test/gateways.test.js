edd7394bf8d81d1ff2af51f9d6e9360aa28cee52
Jared Hanson <jaredhanson@gmail.com>
2020-06-17 15:55:45 -0700

    Add tests.

--- test/gateways.test.js
+++ test/gateways.test.js
@@ -22,7 +22,7 @@ describe('gateway', function() {
     
     var container = new Object();
     container.components = sinon.stub()
-    container.components.withArgs('http://i.bixbyjs.org/http/Gateway').resolves([]);
+    container.components.withArgs('http://i.bixbyjs.org/http/Gateway').returns([]);
     container.create = sinon.stub()
     container.create.withArgs('./gateway/http').resolves(gateway);
     
@@ -39,5 +39,38 @@ describe('gateway', function() {
       expect(gateways[0]).to.equal(gateway);
     });
   }); // when app uses default gateway
+  
+  describe('when gateway components are instantiated', function(done) {
+    var gateways;
+    
+    var gateway1 = new Object();
+    var gateway2 = new Object();
+    
+    var gateway1Component = new Object();
+    gateway1Component.create = sinon.stub().resolves(gateway1);
+    var gateway2Component = new Object();
+    gateway2Component.create = sinon.stub().resolves(gateway2);
+    
+    var container = new Object();
+    container.components = sinon.stub()
+    container.components.withArgs('http://i.bixbyjs.org/http/Gateway').returns([
+      gateway1Component,
+      gateway2Component
+    ]);
+    
+    before(function(done) {
+      factory(container).then(function(obj) {
+        gateways = obj;
+        done();
+      }, done);
+    });
+    
+    it('should provide gateways', function() {
+      expect(gateways).to.be.an('array');
+      expect(gateways).to.have.length(2);
+      expect(gateways[0]).to.equal(gateway1);
+      expect(gateways[1]).to.equal(gateway2);
+    });
+  }); // when gateway components are instantiated
   
 });
