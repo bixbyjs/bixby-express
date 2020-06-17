c43fe483754cf217b0ea14ab0bffdbf1fda65e12
Jared Hanson <jaredhanson@gmail.com>
2020-06-17 15:46:12 -0700

    Add gateways tests.

--- test/gateways.test.js
+++ test/gateways.test.js
@@ -1,5 +1,7 @@
 /* global describe, it, expect */
 
+var expect = require('chai').expect;
+var sinon = require('sinon');
 var factory = require('../app/gateways');
 
 
@@ -11,7 +13,31 @@ describe('gateway', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal(undefined);
-    expect(factory['@singleton']).to.equal(undefined);
   });
+  
+  describe('when app uses default gateway', function(done) {
+    var gateways;
+    
+    var gateway = new Object();
+    
+    var container = new Object();
+    container.components = sinon.stub()
+    container.components.withArgs('http://i.bixbyjs.org/http/Gateway').resolves([]);
+    container.create = sinon.stub()
+    container.create.withArgs('./gateway/http').resolves(gateway);
+    
+    before(function(done) {
+      factory(container).then(function(obj) {
+        gateways = obj;
+        done();
+      }, done);
+    });
+    
+    it('should provide gateways', function() {
+      expect(gateways).to.be.an('array');
+      expect(gateways).to.have.length(1);
+      expect(gateways[0]).to.equal(gateway);
+    });
+  }); // when app uses default gateway
   
 });
