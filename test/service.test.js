072102c31cf6d64b227b6ec549a4c680f12c3360
Jared Hanson <jaredhanson@gmail.com>
2020-06-18 10:42:03 -0700

    Add tests.

--- test/service.test.js
+++ test/service.test.js
@@ -1,6 +1,9 @@
 /* global describe, it, expect */
 
+var expect = require('chai').expect;
+var sinon = require('sinon');
 var factory = require('../app/service');
+var express = require('express');
 
 
 describe('service', function() {
@@ -11,7 +14,48 @@ describe('service', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal(undefined);
-    expect(factory['@singleton']).to.equal(undefined);
   });
+  
+  describe('instantiating with multiple services', function(done) {
+    var app;
+    
+    var service1 = new express.Router();
+    var service2 = new express.Router();
+    
+    var service1Component = new Object();
+    service1Component.create = sinon.stub().resolves(service1);
+    service1Component.a = { '@path': '/beep' };
+    var service2Component = new Object();
+    service2Component.create = sinon.stub().resolves(service2);
+    service2Component.a = { '@path': '/boop' };
+    
+    var container = new Object();
+    container.components = sinon.stub()
+    container.components.withArgs('http://i.bixbyjs.org/http/Service').returns([
+      service1Component,
+      service2Component
+    ]);
+    //container.create = sinon.stub()
+    //container.create.withArgs('http://i.bixbyjs.org/http/Server').resolves(server);
+    
+    var logging = function(req, res, next){};
+    var loggingMiddleware = sinon.stub().returns(logging);
+    
+    var logger = new Object();
+    logger.notice = sinon.spy();
+    logger.debug = sinon.spy();
+    
+    
+    before(function(done) {
+      factory(container, loggingMiddleware, logger).then(function(obj) {
+        app = obj;
+        done();
+      }, done);
+    });
+    
+    it('should provide service', function() {
+      expect(app).to.be.a('function');
+    });
+  }); // instantiating with multiple services
   
 });
