882edcd9967f8de6e6621948a9ee00735b33926d
Jared Hanson <jaredhanson@gmail.com>
2020-10-03 14:53:22 -0700

    Add test case.

--- test/main.test.js
+++ test/main.test.js
@@ -96,6 +96,45 @@ describe('main', function() {
     });
   }); // when app provides app-specific service
   
+  describe('when app provides app-specific service to be mounted by default service', function(done) {
+    var service = sinon.stub(express());
+    
+    var gateway = new Object();
+    gateway.on = sinon.spy();
+    gateway.listen = sinon.stub().yieldsOn(gateway);
+    gateway.address = sinon.stub().returns({ address: '127.0.0.1', port: 8080 });
+    
+    var container = new Object();
+    container.create = sinon.stub()
+    container.create.withArgs('app/service', { meta: true }).resolves([ service, { a: { '@path': '/' } } ]);
+    container.create.withArgs('./service').resolves(service);
+    container.create.withArgs('./gateways').resolves([ gateway ]);
+    
+    var logger = new Object();
+    logger.info = sinon.spy();
+    
+    before(function(done) {
+      factory(container, logger).then(done, done);
+    });
+    
+    it('should create service', function() {
+      expect(container.create).to.be.calledThrice;
+      expect(container.create.getCall(0)).to.be.calledWith('app/service');
+      expect(container.create.getCall(1)).to.be.calledWith('./service');
+      expect(container.create.getCall(2)).to.be.calledWith('./gateways');
+    });
+    
+    it('should dispatch requests from gateway', function() {
+      expect(gateway.on).to.be.calledWith('request', service);
+      expect(gateway.listen).to.be.calledOnce;
+    });
+    
+    it('should log messages', function() {
+      expect(logger.info).to.be.calledOnce;
+      expect(logger.info.getCall(0)).to.be.calledWith('HTTP server listening on %s:%d', '127.0.0.1', 8080);
+    });
+  }); // when app provides app-specific service to be mounted by default service
+  
   describe('when app provides app-specific service that fails to be created', function(done) {
     var service = sinon.stub(express());
     
