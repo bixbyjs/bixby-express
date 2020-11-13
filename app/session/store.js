75a2f5bc7f5e3d5ddf7bfb8f4c142ebaecfe9a80
Jared Hanson <jaredhanson@gmail.com>
2020-11-12 21:06:17 -0800

    Simplify creation of session store.

--- app/session/store.js
+++ app/session/store.js
@@ -1,26 +1,10 @@
-exports = module.exports = function(IoC, connect, logger) {
-  var uri = require('url');
+exports = module.exports = function(IoC, logger) {
   
-  
-  var promise = new Promise(function(resolve, reject) {
-    var modules = IoC.components('http://i.bixbyjs.org/http/ISessionStore')
-      , services = modules.map(function(m) { return m.a['@name']; });
-    
-    // TODO: Introspect container for session service types.
-    //var type = 'sess-redis';
-    //var type = 'sessions-mongodb';
-    
-    //var services = [ 'x-foo' ];
-    
-    connect(services, function(err, conn) {
-      if (err) { reject(err); }
-      resolve(conn);
-    });
-  });
-  
-  return promise
+  return IoC.create('http://i.bixbyjs.org/http/SessionStore')
     .catch(function(err) {
-      if (err.code !== 'ENOTFOUND') { throw err; }
+      console.log(err);
+      
+      //if (err.code !== 'ENOTFOUND') { throw err; }
       if (process.env.NODE_ENV !== 'development') {
         // TODO: Consider using a cookie-based store here, for stateless sessions
         //throw err;
@@ -35,6 +19,5 @@ exports = module.exports = function(IoC, connect, logger) {
 exports['@singleton'] = true;
 exports['@require'] = [
   '!container',
-  'http://i.bixbyjs.org/ns/connect',
   'http://i.bixbyjs.org/Logger'
 ];
