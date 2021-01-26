88f6cd71e79b03c63805512177dabbd98652296b
Jared Hanson <jaredhanson@gmail.com>
2021-01-25 21:22:08 -0800

    Require state store with better interface name.

--- app/middleware/state.js
+++ app/middleware/state.js
@@ -13,5 +13,5 @@ exports['@implements'] = [
   'http://i.bixbyjs.org/http/middleware/state'
 ];
 exports['@require'] = [
-  'http://i.bixbyjs.org/http/state/Store'
+  'http://i.bixbyjs.org/http/StateStore'
 ];
