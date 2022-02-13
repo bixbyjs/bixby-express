98dc9f47a05b3eacb6bc5b304a37137fa189d3bc
Jared Hanson <jaredhanson@gmail.com>
2022-02-13 06:46:59 -0800

    Add tests.

--- test/session/store.test.js
+++ test/session/store.test.js
@@ -40,8 +40,8 @@ describe('session/store', function() {
     
     factory(container, logger)
       .then(function(obj) {
-        expect(container.create).have.been.calledOnce;
-        expect(container.create).have.been.calledWith('http://i.bixbyjs.org/http/SessionStore');
+        expect(container.create).to.have.been.calledOnce;
+        expect(container.create).to.have.been.calledWith('http://i.bixbyjs.org/http/SessionStore');
         expect(obj).to.equal(store);
         done();
       })
@@ -61,7 +61,9 @@ describe('session/store', function() {
     
     factory(container, logger)
       .then(function(obj) {
-        expect(container.create).have.been.calledTwice;
+        expect(logger.notice).to.have.been.calledWith('Using in-memory HTTP session store during development');
+        
+        expect(container.create).to.have.been.calledTwice;
         expect(container.create.getCall(0).args[0]).to.equal('http://i.bixbyjs.org/http/SessionStore');
         expect(container.create.getCall(1).args[0]).to.equal('./store/memory');
         expect(obj).to.equal(store);
@@ -69,5 +71,25 @@ describe('session/store', function() {
       })
       .catch(done);
   }); // should resolve with memory store in development environment when implementation not found
+  
+  it('should re-throw error in production environment when implementation not found', function(done) {
+    process.env.NODE_ENV = 'production';
+    
+    var error = new Error('Cannot find implementation');
+    error.code = 'IMPLEMENTATION_NOT_FOUND';
+    error.interface = 'http://i.bixbyjs.org/http/SessionStore';
+    container.create.withArgs('http://i.bixbyjs.org/http/SessionStore').rejects(error);
+    
+    var store = new Object();
+    container.create.withArgs('./store/memory').resolves(store);
+    
+    factory(container, logger)
+      .catch(function(err) {
+        expect(container.create).to.have.been.calledOnce;
+        expect(container.create).to.have.been.calledWith('http://i.bixbyjs.org/http/SessionStore');
+        expect(err).to.equal(error);
+        done();
+      });
+  }); // should re-throw error in production environment when implementation not found
   
 }); // session/store
