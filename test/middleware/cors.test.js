dbbb71a97de66b1f59ff99ea6fba00e26653aff2
Jared Hanson <jaredhanson@gmail.com>
2022-02-15 18:39:01 -0800

    Add tests.

--- test/middleware/cors.test.js
+++ test/middleware/cors.test.js
@@ -1,5 +1,7 @@
 /* global describe, it, expect */
 
+var expect = require('chai').expect;
+var sinon = require('sinon');
 var factory = require('../../app/middleware/cors');
 
 
@@ -12,6 +14,31 @@ describe('middleware/cors', function() {
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://i.bixbyjs.org/http/middleware/cors');
     expect(factory['@singleton']).to.equal(undefined);
+  });
+  
+  it('should return setup function', function() {
+    setup = factory();
+    expect(setup).to.be.a('function');
+  }); // should return setup function
+  
+  describe('setup function', function() {
+    
+    it('should create middleware with default options', function() {
+      var cors = sinon.stub();
+      cors.returns(function(req, res, next){});
+      
+      var factory = $require('../../app/middleware/cors',
+        { 'cors': cors }
+      );
+      
+      setup = factory();
+      
+      var middleware = setup();
+      expect(cors).to.have.been.calledOnceWithExactly(undefined);
+      expect(middleware).to.be.a('function');
+      expect(middleware.length).to.equal(3);
+    });
+    
   });
   
 });
