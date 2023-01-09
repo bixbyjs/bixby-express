109558ce913756a16308a7e19bc9e37443201ac2
Jared Hanson <jaredhanson@gmail.com>
2023-01-09 08:08:35 -0800

    Add redis session store (copied from bixby-redis).

---
+++ app/session/store/redis.js
@@ -0,0 +1,42 @@
+/**
+ * Redis session store.
+ *
+ * This component provides HTTP session storage using [Redis][1].
+ *
+ * The location of this service will be discovered by looking up the service
+ * name `sess-resp`.  This name is derived from the default key prefix, `sess:`,
+ * used by [`connect-redis`][2] suffixed by `-resp`, the application layer
+ * [protocol][3] implemented by Redis, and thus used by this session store.
+ *
+ * Operationally, the list of sessions stored by Redis can be queried using
+ * `redis-cli`:
+ *
+ *     $ redis-cli
+ *     > KEYS sess:*
+ *
+ * Session data can be read by querying a session ID:
+ *
+ *     > GET sess:b_atMd5ftfIJ7YeBNeBUoH1pgl4KcErs
+ *
+ * [1]: https://redis.io/
+ * [2]: https://github.com/tj/connect-redis
+ * [3]: https://redis.io/topics/protocol
+ */
+exports = module.exports = function(redis, location) {
+  var session = require('express-session');
+  var RedisStore = require('connect-redis')(session);
+  
+  
+  var client = redis.createConnection(location);
+  var store = new RedisStore({ client: client });
+  return store;
+};
+
+exports['@singleton'] = true;
+exports['@implements'] = 'http://i.bixbyjs.org/http/SessionStore';
+exports['@service'] = 'sess-resp';
+exports['@port'] = 6379;
+exports['@require'] = [
+  'http://i.bixbyjs.org/redis',
+  '$location'
+];
---
+++ test/session/store/redis.test.js
@@ -0,0 +1,50 @@
+var $require = require('proxyquire');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var factory = require('../../../app/session/store/redis');
+// TODO: Remove redis dependency from test
+var redis = require('redis');
+var RedisStore = require('connect-redis')(require('express-session'));
+
+
+describe('http/session/store', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@singleton']).to.equal(true);
+    expect(factory['@implements']).to.equal('http://i.bixbyjs.org/http/SessionStore');
+    expect(factory['@service']).to.equal('sess-resp');
+    expect(factory['@port']).to.equal(6379);
+  });
+  
+  describe('creating with defaults', function() {
+    var RedisStoreSpy = sinon.spy(RedisStore);
+    var factory = $require('../../../app/session/store/redis',
+      { 'connect-redis': function() { return RedisStoreSpy; } });
+    
+    var client = sinon.createStubInstance(redis.RedisClient);
+    var _redis = new Object();
+    _redis.createConnection = sinon.stub().returns(client);
+    
+    var store = factory(_redis, { host: 'redis.example.com', port: 6379 });
+  
+    it('should create connection', function() {
+      expect(_redis.createConnection).to.have.been.calledOnce;
+      expect(_redis.createConnection).to.have.been.calledWith({ host: 'redis.example.com', port: 6379 });
+    });
+  
+    it('should construct store', function() {
+      expect(RedisStoreSpy).to.have.been.calledOnce;
+      expect(RedisStoreSpy).to.have.been.calledWithNew;
+      expect(RedisStoreSpy).to.have.been.calledWith({ client: client });
+    });
+  
+    it('should return store', function() {
+      expect(store).to.be.an.instanceOf(RedisStore);
+    });
+  }); // creating with defaults
+  
+}); // http/session/store
