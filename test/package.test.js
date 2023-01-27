09899174895b5a03ad1f9c374b7e4783274947a2
Jared Hanson <jaredhanson@gmail.com>
2023-01-27 06:58:48 -0800

    Move state middleware to attic.

--- test/package.test.js
+++ test/package.test.js
@@ -11,7 +11,7 @@ describe('bixby-express', function() {
     it('should have assembly metadata', function() {
       expect(json.assembly.namespace).to.equal('http');
       
-      expect(json.assembly.components).to.have.length(8);
+      expect(json.assembly.components).to.have.length(7);
       expect(json.assembly.components).to.include('main');
       expect(json.assembly.components).to.include('middleware/errorlogging');
       expect(json.assembly.components).to.include('session/stores/memory');
