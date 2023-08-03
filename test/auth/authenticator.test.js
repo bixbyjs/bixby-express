cc406d6930361a989265a490a56f252f3b5509c9
Jared Hanson <jaredhanson@gmail.com>
2023-08-03 15:37:21 -0700

    Stub in test.

--- test/auth/authenticator.test.js
+++ test/auth/authenticator.test.js
@@ -1,7 +1,10 @@
 /* global describe, it, expect */
 
-var factory = require('../../app/auth/authenticator');
 var expect = require('chai').expect;
+var $require = require('proxyquire');
+var sinon = require('sinon');
+var factory = require('../../app/auth/authenticator');
+var Authenticator = require('passport').Authenticator;
 
 
 describe('auth/authenticator', function() {
@@ -13,6 +16,20 @@ describe('auth/authenticator', function() {
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('module:passport.Authenticator');
     expect(factory['@singleton']).to.equal(true);
+  });
+
+  // TODO:
+  it.skip('should construct Authenticator', function() {
+    var AuthenticatorSpy = sinon.spy(Authenticator);
+    var factory = $require('../../app/auth/authenticator', {
+      'passport': { Authenticator: AuthenticatorSpy }
+    });
+    
+    var authenticator = factory();
+    
+    expect(AuthenticatorSpy).to.have.been.calledOnce;
+    expect(AuthenticatorSpy).to.have.been.calledWithNew;
+    // TODO: Assertions for using scheme and manager
   });
   
 });
